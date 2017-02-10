acfb6f24df45884f91523599d2fe515bb90ea930
pengju-wang <526527853@qq.com>
2017-02-10 11:19:47 +0800

    Fixed some bugs

--- nodebaidu/routes/db.js
+++ nodebaidu/routes/db.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'Ourtime',
-    password: '526527853',
+    password: 'admin',
     database: 'baidunews'
 });
 
